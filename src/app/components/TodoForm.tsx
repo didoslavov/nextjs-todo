6b29133fcf3f190fea459bba6426e0db0660c070
didoslavov <didkoslawow@gmail.com>
2024-04-02 02:27:57 +0300

    use server action for create todo

--- src/app/components/TodoForm.tsx
+++ src/app/components/TodoForm.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useRef } from 'react';
-import { createTodo } from '../../../lib/todos';
+import { createTodoAction } from '../_actions';
 
 const TodoForm = () => {
     const formRef = useRef<HTMLFormElement>(null);
@@ -9,17 +9,24 @@ const TodoForm = () => {
     const formAction = async (formData: FormData) => {
         const title = formData.get('title');
 
+        console.log(title);
+
         if (typeof title !== 'string' || !title) {
             return;
         }
 
-        await createTodo(title);
+        await createTodoAction(title);
         formRef.current?.reset();
     };
 
     return (
         <form ref={formRef} action={formAction} className="flex space-x-6 mb-12">
-            <input type="text" className="rounded-md p-2 bg-teal-100 placeholder:text-teal-600" placeholder="Todo..." />
+            <input
+                name="title"
+                type="text"
+                className="rounded-md p-2 bg-teal-100 placeholder:text-teal-600"
+                placeholder="Todo..."
+            />
             <button className="rounded bg-teal-200 text-teal-900 p-2">Add todo</button>
         </form>
     );
