adbeb4211d82b4c22d252e16019b9cdd8889411f
didoslavov <didkoslawow@gmail.com>
2024-04-02 02:27:42 +0300

    add server actions form create and update todo

---
+++ src/app/_actions.ts
@@ -0,0 +1,14 @@
+'use server';
+
+import { revalidatePath } from 'next/cache';
+import { createTodo, updateTodo } from '../../lib/todos';
+
+export async function createTodoAction(title: string) {
+    await createTodo(title);
+    revalidatePath('/');
+}
+
+export async function updateTodoAction(id: string, isCompleted: boolean) {
+    await updateTodo(id, isCompleted);
+    revalidatePath('/');
+}
