fd22299638f1f48a68fc4e295b2aaa344364e6c0
didoslavov <didkoslawow@gmail.com>
2024-04-02 20:04:44 +0300

    style updatedAt

--- src/app/components/TodoItem.tsx
+++ src/app/components/TodoItem.tsx
@@ -2,7 +2,7 @@
 
 import { useTransition } from 'react';
 import { Todo } from '@prisma/client';
-import { updateTodoAction } from '../_actions';
+import { deleteTodoAction, updateTodoAction } from '../_actions';
 import { RiDeleteBin7Line } from 'react-icons/ri';
 
 type TodoItemProps = {
@@ -14,17 +14,25 @@ const TodoItem = ({ todo }: TodoItemProps) => {
 
     return (
         <div className="flex gap-4">
-            <RiDeleteBin7Line size={25} className="text-teal-900 mt-2 hover:cursor-pointer hover:text-pink-950" />
             <li
                 onClick={() => startTransition(() => updateTodoAction(todo.id, !todo.isCompleted))}
                 className={`${
                     todo.isCompleted
                         ? 'bg-teal-900 transition-colors duration-300 text-teal-100'
                         : 'transition-colors duration-300'
-                } flex justify-between mb-4 border border-teal-600 py-2 px-4 rounded cursor-pointer gap-6`}>
+                } w-full flex justify-between mb-4 border border-teal-600 py-2 px-4 rounded cursor-pointer gap-6`}>
                 <p>{todo.title}</p>
-                <span>{todo.updatedAt.toISOString()}</span>
+                <span>
+                    {todo.updatedAt
+                        .toISOString()
+                        .slice(todo.updatedAt.toISOString().indexOf('T') + 1, todo.updatedAt.toISOString().indexOf('.'))}
+                </span>
             </li>
+            <RiDeleteBin7Line
+                onClick={() => deleteTodoAction(todo.id)}
+                size={25}
+                className="text-teal-900 mt-2 hover:cursor-pointer hover:text-pink-950"
+            />
         </div>
     );
 };
