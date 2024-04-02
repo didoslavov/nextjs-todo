7b1ec5dbeb62763f8a85282e369ee11b18d1aa26
ntzolov <ntzolov@gmail.com>
2024-04-02 20:18:28 +0300

    styles change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,6 @@ export const dynamic = 'force-dynamic';
 export default async function Home() {
   const { todos } = await getTodos();
 
-<<<<<<< HEAD
   return (
     <section className='flex items-center justify-center bg-teal-50 h-screen placeholder:bg-inherit '>
       <div className='flex flex-col w-fit border-2 border-amber-600 p-10 bg-amber-50'>
@@ -23,21 +22,4 @@ export default async function Home() {
       </div>
     </section>
   );
-=======
-    return (
-        <section className="flex flex-col items-center justify-center bg-teal-50 h-screen placeholder:bg-inherit">
-            <h1 className="font-extrabold text-6xl mb-12">Todos</h1>
-            <div className="flex flex-col w-fit">
-                <TodoForm />
-                <article className="w-full">
-                    <ul>
-                        {todos?.map((t) => (
-                            <TodoItem key={t.id} todo={t} />
-                        ))}
-                    </ul>
-                </article>
-            </div>
-        </section>
-    );
->>>>>>> 2c5779ae0d05f4defff368f7458dc00929fe7095
 }
